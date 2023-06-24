230bf1f76944ef112781c5f99441773e6aa9edc6
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-06-24 17:51:11 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-Dark-Emerald p-4 container mx-auto">
-    <header>
-      <nav>
+  <div class="bg-Dark-Emerald">
+    <header class="p-4 lg:max-w-5xl mx-auto">
+      <nav class="container mx-auto sm:flex sm:justify-between">
         <div class="flex justify-between text-white">
           <router-link to="/">
             <span>Worldwide Cuisine Explorer</span></router-link
@@ -9,29 +9,48 @@
 
           <div :class="['navbar', { 'navbar-open': isMenuOpen }]">
             <div class="cursor-pointer" @click="toggleMenu">
-              <span class="w-6 h-1 mb-1 block bg-white"></span>
-              <span class="w-6 h-1 mb-1 block bg-white"></span>
-              <span class="w-6 h-1 block bg-white"></span>
+              <span
+                :class="{ 'rotate-45 translate-x-1 translate-y-3': isMenuOpen }"
+                class="w-6 h-1 mb-1 block bg-white"
+              ></span>
+              <span
+                :class="{ hidden: isMenuOpen }"
+                class="w-6 h-1 mb-1 block bg-white"
+              ></span>
+              <span
+                :class="{
+                  '-rotate-45 translate-x-1 translate-y-1': isMenuOpen,
+                }"
+                class="w-6 h-1 block bg-white"
+              ></span>
             </div>
           </div>
         </div>
 
         <div>
-          <ul :class="{ show: isMenuOpen }">
+          <ul
+            class="text-white sm:flex max-sm:bg-black max-sm:mt-5 max-sm:p-5 max-sm:text-center max-sm:text-lg max-sm: max-sm:rounded-br-2xl max-sm:rounded-bl-2xl max-sm:w-full"
+            :class="{ show: isMenuOpen }"
+          >
+            <li class="sm:mr-3">
+              <router-link to="/about">Search</router-link>
+            </li>
+            <li class="sm:mr-3">
+              <router-link to="/">Home</router-link>
+            </li>
             <li><router-link to="/about">About</router-link></li>
           </ul>
         </div>
       </nav>
     </header>
 
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 @import url("./styles/app.css");
+@import url("./styles/navigation.css");
 </style>
 
-<script src="@/composables/navigation">
-
-</script>
+<script src="@/composables/navigation"></script>
